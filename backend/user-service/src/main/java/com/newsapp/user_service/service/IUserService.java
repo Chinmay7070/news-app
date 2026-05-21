3d70d09508b5b89652dedfdac111ca1cccb9f114
Chinmay Chaudhari <chaudharichinmay227@gmail.com>
2026-05-21 15:12:45 +0530

    user-service: added verify otp method

--- backend/user-service/src/main/java/com/newsapp/user_service/service/IUserService.java
+++ backend/user-service/src/main/java/com/newsapp/user_service/service/IUserService.java
@@ -2,8 +2,10 @@ package com.newsapp.user_service.service;
 
 import com.newsapp.user_service.dto.LoginRequest;
 import com.newsapp.user_service.dto.RegisterRequest;
+import com.newsapp.user_service.dto.VerifyOtpRequest;
 
 public interface IUserService {
     String resiter(RegisterRequest request);
     String login(LoginRequest request);
+    public String verifyOtp(VerifyOtpRequest request);
 }
