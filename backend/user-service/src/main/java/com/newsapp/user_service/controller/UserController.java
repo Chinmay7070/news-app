c080499adf1cf911d1f6896942b5b86a22d58ec2
Chinmay Chaudhari <chaudharichinmay227@gmail.com>
2026-05-21 14:44:00 +0530

    user-service: added otp feature with email service

--- backend/user-service/src/main/java/com/newsapp/user_service/controller/UserController.java
+++ backend/user-service/src/main/java/com/newsapp/user_service/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserServiceImpl userService;
+    private final IUserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
